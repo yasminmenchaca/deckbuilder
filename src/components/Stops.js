73990a3cf8dab2890a27cd01870f136ec23d65ae
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2021-02-09 12:03:25 -0600

    buttons change on click, added styling

--- src/components/Stops.js
+++ src/components/Stops.js
@@ -1,12 +1,48 @@
-import React, { Component } from "react";
-import { Container, Form } from "react-bootstrap";
+import React from "react";
+import { Container } from "react-bootstrap";
+import "../App.css";
 
-export default class Stops extends Component {
-  render() {
-    return (
-      <div>
-        <Container></Container>
-      </div>
-    );
-  }
-}
+const Button = (props) => {
+  const [color, setColors] = React.useState("");
+  const [active, setActive] = React.useState(false);
+  const handleClickButton = (name) => {
+    setActive(true);
+    setColors(name);
+    if (active === true) {
+      setActive(false);
+      setColors("button");
+    }
+  };
+
+  console.log(active);
+  return (
+    <button
+      className={`button ${color}`}
+      onClick={() => handleClickButton(props.name)}
+    >
+      {props.name}
+    </button>
+  );
+};
+
+const Stops = () => {
+  const [names, setNames] = React.useState([
+    "Power",
+    "Technique",
+    "Agility",
+    "Strike",
+    "Submission",
+    "Grapple",
+  ]);
+
+  return (
+    <div>
+      <Container>
+        {names.map((name) => (
+          <Button key={name} name={name} />
+        ))}
+      </Container>
+    </div>
+  );
+};
+export default Stops;
